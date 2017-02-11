c264761e76f26049d9b5c912836843a784a7f5b7
Guillermo Lopez <guillermolopez2529@gmail.com>
2017-02-11 11:23:22 -0400

    Added changed

--- api/users.js
+++ api/users.js
@@ -46,7 +46,7 @@ async function main(req, res) {
       send(res, 500, { err: e.message });
     }
   } else {
-    send(res, 404, { err: 'route not found' })
+    send(res, 404, { err: 'route not found' });
   }
 }
 
--- websocket/index.js
+++ websocket/index.js
@@ -3,7 +3,7 @@ const { createServer } = require('http');
 
 const server = createServer();
 const io = socketio(server);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 io.on('connection', (socket) => {
   console.log('a user connected');
